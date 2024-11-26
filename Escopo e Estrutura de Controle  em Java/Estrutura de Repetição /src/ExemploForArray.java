ccaac8626867bdb278188a1fa4f25fe82059e52f
Joabe Costa <j.hoab@hotmail.com>
2024-11-25 21:48:55 -0300

    ExemploForArray

---
+++ Escopo e Estrutura de Controle  em Java/Estrutura de Repetição /src/ExemploForArray.java
@@ -0,0 +1,14 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Felipe","Jonas","Julia","Marcos"};
+        //o indice em array inicia em zero 
+        /*for(int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no indice x= "+x+" é "+alunos[x]); }*/
+        for(String aluno:alunos){
+            System.out.println("O nome do aluno é: "+aluno);
+          }  
+        }
+
+
+    }
+
